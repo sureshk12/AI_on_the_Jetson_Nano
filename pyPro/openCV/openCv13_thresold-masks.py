0e81ea9a3b6d83feb08a09e6c696ccd8c89b2c84
Suresh Kumar <suresh@ashank.com>
2020-12-25 11:29:03 +0530

    OpenCv13 was updated

--- pyPro/openCV/openCv13_thresold-masks.py
+++ pyPro/openCV/openCv13_thresold-masks.py
@@ -11,10 +11,10 @@ def dummyCallback(x):
 dispW = 640
 dispH = 480
 flip = 2
-# camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=1848, format=NV12, framerate=28/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-# cam = cv2.VideoCapture(camSet)
+camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=1848, format=NV12, framerate=28/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam = cv2.VideoCapture(camSet)
 
-cam = cv2.VideoCapture(0)
+# cam = cv2.VideoCapture(0)
 
 cvImg = cv2.imread('cv.jpg')
 cvImg = cv2.resize(cvImg, (320, 240))
