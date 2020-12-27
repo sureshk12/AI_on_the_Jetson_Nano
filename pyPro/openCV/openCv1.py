be6d4bcaa6aca13aa9adac905238a3fdfad0286c
Suresh Kumar Nano <suresh@ashank.com>
2020-12-27 12:06:04 +0530

    openCv1 updated

--- pyPro/openCV/openCv1.py
+++ pyPro/openCV/openCv1.py
@@ -5,7 +5,7 @@ print(cv2.__version__)
 dispW = 640
 dispH = 480
 flip = 2
-camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=1848, format=NV12, framerate=28/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=NV12, framerate=21/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
 cam = cv2.VideoCapture(camSet)
 
 
@@ -17,7 +17,7 @@ while True:
 
     cv2.imshow('Camera', frame)
     cv2.moveWindow('Camera', 0, 0)
-    keyPressed = cv2.waitKey(33)
+    keyPressed = cv2.waitKey(20)
     if keyPressed == ord('q'):
         break
 
