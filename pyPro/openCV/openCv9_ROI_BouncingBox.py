6629a6aa0803d3f49684f382e8bbd1dfbb1d00bc
Suresh Kumar Nano <suresh@ashank.com>
2020-12-31 17:30:15 +0530

    Updated Nano Files

--- pyPro/openCV/openCv9_ROI_BouncingBox.py
+++ pyPro/openCV/openCv9_ROI_BouncingBox.py
@@ -6,11 +6,11 @@ print(cv2.__version__)
 # ***Rapberry Pi Camera setting***
 dispW = 640
 dispH = 480
-# flip = 2
-# camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=1848, format=NV12, framerate=28/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
-# cam = cv2.VideoCapture(camSet)
+flip = 2
+camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=1848, format=NV12, framerate=28/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink'
+cam = cv2.VideoCapture(camSet)
 
-cam = cv2.VideoCapture(0)
+# cam = cv2.VideoCapture(0)
 rectW = 160
 rectH = 120
 posX = random.randint(0, dispW - rectW)
