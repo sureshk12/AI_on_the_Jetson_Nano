5146dde57bd3de2defa6b90b2be6c7cab3d1f8ae
Suresh Kumar <suresh@ashank.com>
2021-01-10 10:05:07 +0530

    Just updated with minor changes

--- pyPro/openCV/openCv1.py
+++ pyPro/openCV/openCv1.py
@@ -1,4 +1,5 @@
 import cv2
+
 print(cv2.__version__)
 
 # ***Rapberry Pi Camera setting***
@@ -8,14 +9,13 @@ flip = 2
 camSet = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=1280, height=720, format=NV12, framerate=10/1 ! nvvidconv flip-method='+str(flip)+' ! video/x-raw, width='+str(dispW)+', height='+str(dispH)+', format=BGRx ! videoconvert ! video/x-raw, format=BGR ! appsink '
 cam = cv2.VideoCapture(camSet)
 
-
 # cam = cv2.VideoCapture(0)
 
 while True:
     ret, frame = cam.read()
     frame = cv2.flip(frame, 1)
     frameNew = frame[:, 0:390]
-    cv2.im
+    
 
     cv2.imshow('Camera', frameNew)
     cv2.moveWindow('Camera', 0, 0)
